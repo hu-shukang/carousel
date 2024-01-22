696601dc0f8ac59175903076dde78c64da1bff61
胡书康 <hushukang@gmail.com>
2024-01-22 18:44:01 +0900

    first commit

---
+++ src/types/carousel.type.ts
@@ -0,0 +1,51 @@
+export type CarouselItem = {
+  id: string;
+  image: string;
+  name: string;
+  desc: string;
+};
+
+export const carouselList: CarouselItem[] = [
+  {
+    id: '1',
+    image: '/images/01.jpg',
+    name: 'Macaca mulatta',
+    desc: '北インドを中心に，中国・ミャンマーにかけて分布しています。ニホンザルとよく似ていますが，アカゲザルの尾はニホンザルよりも長く，体長の30～45％ほどもあります。',
+  },
+  {
+    id: '2',
+    image: '/images/02.jpg',
+    name: 'Ailurus fulgens',
+    desc: 'レッサーパンダは，中国南部に生息するシセンレッサーパンダと，ヒマラヤからミャンマー北部に分布するネパールレッサーパンダという2つの亜種に分類されます。',
+  },
+  {
+    id: '3',
+    image: '/images/03.jpg',
+    name: 'Monticola solitarius',
+    desc: 'ヒヨドリと大きさが似ており，海岸近くの磯でも見られることからこの名前がつきました。繁殖期の春には美しい旋律でさえずります。オスは頭から胸，背，腰までが青藍色ですが，メスは全体が灰褐色で，腹にうろこ模様があります。',
+  },
+  {
+    id: '4',
+    image: '/images/04.jpg',
+    name: 'Oryctolagus cuniculus',
+    desc: 'ヨーロッパ原産のアナウサギを改良してつくられた。足の裏には厚く柔らかい体毛が生えている。耳にはたくさんの血管が通っており，体温が上昇した時に体の熱を放出する体温調節の働きをする。',
+  },
+  {
+    id: '5',
+    image: '/images/05.jpg',
+    name: 'Cavia porcellus',
+    desc: '野生種の原産はペルー・ブラジル・ボリビアなどです。妊娠期間が2か月と他のネズミより長く，生まれたときから目も開き，毛も生えています。モルモットとも呼ばれ，ペットとして家庭でも飼育されています。',
+  },
+  {
+    id: '6',
+    image: '/images/06.jpg',
+    name: 'Hippopotamus amphibius',
+    desc: 'サハラ砂漠以南に分布しています。体重はゾウに次いで重く，約2～3.2ｔになります。 皮膚を乾燥から守るために，陸上にあがると赤い液体を分泌します。',
+  },
+  {
+    id: '7',
+    image: '/images/07.jpg',
+    name: 'Cynops pyrrhogaster',
+    desc: 'ニホンイモリとも呼ばれ，日本の固有種です。全長8～13cm。種名の通り，腹側が赤いのが特徴的なイモリです。流れのない淡水中に生息し，昆虫やミミズなどの小動物をエサとしています。',
+  },
+];
